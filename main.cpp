43ac3b0d382fab337e5d3b106980be7d5b46be02
Definitelynotaspruce <justjolantux@gmail.com>
2020-02-14 15:52:07 +0200

    versija 1.0?

--- main.cpp
+++ main.cpp
@@ -3,28 +3,39 @@
 
 using namespace std;
 
+void plotis ( int sk, string t);
+
 int main()
 {
     string vardas;
-    int sk;
+    int sk, papildomas;
+
     cout<<"Iveskite savo varda:"<<endl;
     cin>>vardas;
-    
-    string tekstukas = " Sveika, " + vardas + "! ";
+    cout<<"Iveskite norima eiluciu skaiciu :"<<endl;
+    cin>>sk;
+    papildomas = (sk - 3) / 2;
 
+    string tekstukas = " Sveika, " + vardas + "! ";
     if (vardas.back() == 's')
     tekstukas = " Sveikas, " + vardas + "! ";
         
     string tarpukas(tekstukas.size(), ' ');
-        
+       
     string zvaigzdutes(tekstukas.size()+2, '*');
 
     cout<<zvaigzdutes<<endl;
-    cout<<"*"<<tarpukas<<"*"<<endl;
+    plotis(papildomas, tekstukas);
     cout<<"*"<<tekstukas<<"*"<<endl;
-    cout<<"*"<<tarpukas<<"*"<<endl;
+    plotis(papildomas, tekstukas);
     cout<<zvaigzdutes<<endl;
-    
-    return 0;
 
+    return 0;
+}
+
+void plotis ( int sk, string t)
+{
+    for (int i=0; i<sk; i++){
+        cout <<  "* " << string(t.size()-2, ' ') << " *" << endl;
+    }
 }
